c24d475de0bf43167430d3f850ff787141ee8b00
unknown <nazrulislam66188@gmail.com>
2023-07-31 10:50:45 +0600

    server side auth implemented

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const cors = require('cors');
 
+const authRoutes = require('./routes/auth.js');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,6 +15,8 @@ app.use(express.urlencoded());
 app.get('/', (req, res) => {
     res.send('Hello from chatter server');
 });
+
+app.use('/auth', authRoutes);
 
 app.listen(port, () => {
     console.log(`Server is running on port:${port}`);
